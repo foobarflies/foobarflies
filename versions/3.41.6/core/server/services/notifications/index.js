8f431b427f91053c272ee0a99c509a637bd240ba
tchapi <regbasket+github@gmail.com>
2021-08-21 20:59:51 +0200

    Remove legacy versions

--- versions/3.41.6/core/server/services/notifications/index.js
+++
@@ -1,6 +0,0 @@
-const settingsCache = require('../settings/cache');
-const {i18n} = require('../../lib/common');
-const ghostVersion = require('../../lib/ghost-version');
-const Notifications = require('./notifications');
-
-module.exports.notifications = new Notifications({settingsCache, i18n, ghostVersion});
--- versions/3.42.0/core/server/api/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./v2');
-module.exports.v2 = require('./v2');
-module.exports.canary = require('./canary');
-module.exports.v3 = require('./v3');
-module.exports.shared = require('./shared');
